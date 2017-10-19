bfd59e13ab293bbbb1f437571953345c8607ddb1
Clinton Wang <clintonjwang@gmail.com>
2017-10-19 19:38:12 -0400

    validation set is from originals

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
 		self.aug_factor = 40
 
 		self.model_path = "model.hdf5"
-		self.aug_dir = "..\\liver-mr-processor\\aug_imgs\\"
-		self.orig_dir = "..\\liver-mr-processor\\orig_imgs\\"
+		self.aug_dir = "aug_imgs\\"
+		self.orig_dir = "orig_imgs\\"
