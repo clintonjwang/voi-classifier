47397cbfb66d546754ab016ab8bc38c0d4b3200b
Clinton Wang <clintonjwang@gmail.com>
2017-10-19 15:48:05 -0400

    checkpoint before merge with liver-mr-processor

--- config.py
+++ config.py
@@ -3,4 +3,8 @@ class Config:
 		self.dims = [24,24,12]
 		self.nb_channels = 1
 		self.nb_classes = 4
+		self.aug_factor = 40
+
 		self.model_path = "model.hdf5"
+		self.aug_dir = "..\\liver-mr-processor\\aug_imgs\\"
+		self.orig_dir = "..\\liver-mr-processor\\orig_imgs\\"
