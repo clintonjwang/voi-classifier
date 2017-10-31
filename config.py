22473d20bf55233106a5212989b13b89b3ccfbd0
Clinton Wang <clintonjwang@gmail.com>
2017-10-31 10:07:22 -0400

    adenomas, flipz

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ class Config:
 	def __init__(self):
 		self.dims = [24,24,12]
 		self.nb_channels = 1
-		self.nb_classes = 4
-		self.aug_factor = {"cyst": 100, "hcc": 100, "hemangioma": 100, "fnh": 100}
+		self.nb_classes = 5
+		self.aug_factor = {"cyst": 100, "hcc": 100, "hemangioma": 100, "fnh": 100, "adenoma": 100}
 
 		self.model_path = "model.hdf5"
 		self.aug_dir = "aug_imgs\\"
