cc88ea7cf4ce1f81fb20795d358b9f4eff510e4a
Clinton Wang <clintonjwang@gmail.com>
2017-11-22 09:14:27 -0500

    refix alignment, 82% acc 4cls

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
 		self.run_num = 2
 		self.dims = [36,36,12]
 		self.nb_channels = 3
-		self.nb_classes = 2#4
+		self.nb_classes = 4
 		#self.vox_dims = [1.25, 1.25, 2.5]
 		self.aug_factor = 50#{"cyst": 65, "hcc": 65, "hemangioma": 75, "fnh": 70, "adenoma": 70, "colorectal": 70, "cholangio": 70}
 		self.train_frac = {"cyst": .8, "fnh": .8, "hcc": .8, "hemangioma": .8, 'adenoma': .8, "colorectal": .8, "cholangio": .8}
@@ -17,7 +17,7 @@ class Config:
 		self.small_voi_path = "small_vois.csv"
 
 		self.full_img_dir = "Z:\\INPUT\\full_imgs_origdims"
-		self.aug_dir = "Z:\\INPUT\\aug_imgs2\\"
-		self.orig_dir = "Z:\\INPUT\\orig_imgs2\\"
-		self.crops_dir = "Z:\\INPUT\\unscaled_training_data2\\"
+		self.crops_dir = "imgs\\unscaled_training_data4\\" #Z:\\INPUT\\
+		self.aug_dir = "imgs\\aug_imgs4\\"
+		self.orig_dir = "imgs\\orig_imgs4\\"
 		self.output_img_dir = "Z:\\OUTPUT\\11-21"
