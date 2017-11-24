cfda7c521a22c7115c3e9e54812ac49b6933ac89
Clinton Wang <clintonjwang@gmail.com>
2017-11-23 21:49:42 -0500

    5cls 74%

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
 		self.run_num = 2
 		self.dims = [36,36,12]
 		self.nb_channels = 3
-		self.nb_classes = 4
+		self.nb_classes = 5
 		#self.vox_dims = [1.25, 1.25, 2.5]
 		self.aug_factor = 50#{"cyst": 65, "hcc": 65, "hemangioma": 75, "fnh": 70, "adenoma": 70, "colorectal": 70, "cholangio": 70}
 		self.train_frac = {"cyst": .8, "fnh": .8, "hcc": .8, "hemangioma": .8, 'adenoma': .8, "colorectal": .8, "cholangio": .8}
@@ -18,6 +18,6 @@ class Config:
 
 		self.full_img_dir = "Z:\\INPUT\\full_imgs_origdims"
 		self.crops_dir = "imgs\\unscaled_training_data4\\" #Z:\\INPUT\\
-		self.aug_dir = "imgs\\aug_imgs_ref\\"
+		self.aug_dir = "imgs\\aug_imgs4\\"
 		self.orig_dir = "imgs\\orig_imgs4\\"
-		self.output_img_dir = "Z:\\OUTPUT\\11-22"
+		self.output_img_dir = "Z:\\OUTPUT\\11-23"
