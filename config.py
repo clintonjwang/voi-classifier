fabf83fd0fe508aa3e24370c8b6f39b0bce7c4f7
Clinton Wang <clintonjwang@gmail.com>
2017-12-02 13:15:18 -0500

    batch training

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ class Config:
 		self.run_num = 2
 		self.dims = [36,36,12]
 		self.nb_channels = 3
-		self.nb_classes = 5
+		#self.nb_classes = 5
 		#self.vox_dims = [1.25, 1.25, 2.5]
 		self.aug_factor = 50#{"cyst": 65, "hcc": 65, "hemangioma": 75, "fnh": 70, "adenoma": 70, "colorectal": 70, "cholangio": 70}
-		self.train_frac = {"cyst": .8, "fnh": .8, "hcc": .8, "hemangioma": .8, 'adenoma': .8, "colorectal": .8, "cholangio": .8}
+		self.train_frac = .6
+		#self.train_frac = {"cyst": .8, "fnh": .8, "hcc": .8, "hemangioma": .8, 'adenoma': .8, "colorectal": .8, "cholangio": .8}
 
 		self.model_path = "model.hdf5"		
 		self.art_voi_path = "voi_art.csv"
